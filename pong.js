8ad5fae2d42d8a4c71faae88bdfc1c939ebd6c68
VictorNative508 <278083444+VictorNative508@users.noreply.github.com>
2026-07-05 01:31:55 -0400

    Created skyblue border and player 1 Rectangle

--- pong.js
+++ pong.js
@@ -4,9 +4,22 @@ let boardWidth = 500;
 let boardHeight = 500;
 let context;
 
+let playerWidth = 10;
+let playerHeight = 50;
+
+let player1 = {
+    x : 10,
+    y : boardHeight/2,
+    width : playerWidth,
+    height : playerHeight
+}
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
     board.width = boardWidth;
     context = board.getContext("2d");
+
+    context.fillStyle = "skyblue";
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
 }
